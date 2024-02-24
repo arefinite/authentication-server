47d8bb6ec9ca04c345918a1a1aa79b4c4bcc2c9b
arefinite <arefinite@gmail.com>
2024-02-24 20:25:33 +0600

    deploy

--- src/api/app.ts
+++ src/api/app.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    allowedHeaders: ['get', 'post', 'put', 'delete','patch']
+    allowedHeaders: '*'
   })
 )
 if (process.env.NODE_ENV === 'DEVELOPMENT') {
