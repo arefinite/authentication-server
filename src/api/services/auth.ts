15274387ea7c14a5c590bc947b181eb5b8cd7d72
arefinite <arefinite@gmail.com>
2024-02-24 20:12:59 +0600

    deploy

--- src/api/services/auth.ts
+++ src/api/services/auth.ts
@@ -1,17 +1,16 @@
-import { Response } from 'express';
-import  jwt  from 'jsonwebtoken';
-
+import { Response } from 'express'
+import jwt from 'jsonwebtoken'
 
 export const generateToken = (res: Response, id: string) => {
   //generate token
   const token = jwt.sign({ userId: id }, process.env.JWT_SECRET_KEY as string, {
-    expiresIn: '1d'
+    expiresIn: '1d',
   })
   //store token in cookie
   res.cookie('auth_token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'PRODUCTION',
-    sameSite: 'lax'
-    
+    sameSite: 'none',
+    domain: process.env.FRONTEND_URL,
   })
 }
